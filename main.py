9f7753876646dd2aa558a2e6c56b3c61ec70cb5f
steve <steve.jones763@gmail.com>
2023-03-26 18:17:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()  # Load all the ENV variables into your os enviroment.
+openai.api_key = os.getenv("OPENAI_API_KEY")  # Get your API key from env variable
+openai.api_key = "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"
+
+reply_count = 0
+msgs = []
+
+session_name= input("Enter a unique session name: ")
+os.makedirs(session_name)
+
+assistant_type = input("What type of chatbot would you like to create?: ")
+msgs.append({"role": "system", "content": assistant_type})
+msgs.append({"role": "user", "content": "introduce yourself"})
+introduction = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=msgs
+)
+introduction_response = introduction["choices"][0]["message"]["content"]
+print(introduction_response)
+
+print("Say hello to your new chatbot! Type quit() when done.")
+
+while True:
+    msg = input("YOU: ")
+    if "quit()" in msg:
+        break
+    msgs.append({"role": "user", "content": msg})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=msgs
+    )
+    reply = response["choices"][0]["message"]["content"]
+    with open(f"{session_name}/response-{reply_count}.md", "x") as file:
+        file.write("Question:\n")
+        file.write(msg)
+        file.write("\n\nAnswer:\n")
+        file.write(reply)
+        file.close()
+
+    msgs.append({"role": "assistant", "content": reply})
+    reply_count += 1
+    print("\nAI: " + reply + "\n")
